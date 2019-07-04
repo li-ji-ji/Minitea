a6030e1b557e66435d8433a5bc77e688a75e81a1
11873 <11873@evil-scream>
2019-07-04 09:44:36 +0800

    第一次上传测试-lhj

---
+++ Minitea/src/main/java/com/lhj/Minitea/MiniteaApplication.java
@@ -0,0 +1,13 @@
+package com.lhj.Minitea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniteaApplication.class, args);
+	}
+
+}
